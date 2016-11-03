8adf6aeee19a1a68c67cec4bb2b540d936637fab
Timo Selvaraj <timoselvaraj@Timos-MacBook-Pro.local>
2016-11-03 11:13:58 -0400

    Exercise 5

--- exercise4.py
+++ exercise4.py
@@ -1,5 +1,8 @@
 from TwitterSearch import *
 
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+
 import pandas as pd
 from pandas import ExcelWriter
 
@@ -11,35 +14,42 @@ try:
     tso.set_language('en')  # we want to see English tweets only
     tso.set_include_entities(True)  # and don't give us all those entity information
 
+
     # Please create a Twitter app key here https://apps.twitter.com
     ts = TwitterSearch(
-        consumer_key='XXXXXXX',
-        consumer_secret='XXXXXXXX',
+        consumer_key='XXXXXX',
+        consumer_secret='XXXXXX',
         access_token='XXXXXXXXXXXX',
-        access_token_secret='XXXXXXXXXX'
+        access_token_secret='XXXXXXXX'
     )
 
     count = 1
     # iterate through the tweets
     for tweet in ts.search_tweets_iterable(tso):
-        # print('@%s tweeted: %s' % (tweet['user']['screen_name'], tweet['text']))
+        #print('@%s tweeted: %s' % (tweet['user']['screen_name'], tweet['text']))
 
-        tweet_dict[count] = [str(tweet['id']), tweet['user']['screen_name'], tweet['text']]
+        blob = TextBlob(tweet['text'], analyzer=NaiveBayesAnalyzer())
+        #print (tweet['text'])
+        #print (blob.sentiment)
+        tweet_dict[count] = [str(tweet['id']), tweet['user']['screen_name'], tweet['text'], blob.sentiment[0]]
+        print(tweet_dict[count])
         count = count + 1
 
-    # print (tweet_dict)
+
+    #print (tweet_dict)
     df = pd.DataFrame.from_dict(tweet_dict, orient='index')
     df.rename(columns={0: 'TweetID'}, inplace=True)
     df.rename(columns={1: 'User'}, inplace=True)
     df.rename(columns={2: 'Tweet'}, inplace=True)
+    df.rename(columns={3: 'Sentiment'}, inplace=True)
 
-    print(df)
+    print (df)
 
 except TwitterSearchException as e:  # take care of all those errors
     print(e)
 
-# save to xls
+#save to xls
 
-writer = ExcelWriter('tweets.xlsx')
-df.to_excel(writer, 'Sheet1')
+writer = ExcelWriter('tweets_sentiment.xlsx')
+df.to_excel(writer,'Sheet1')
 writer.save()
